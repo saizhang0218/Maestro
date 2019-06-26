3dd10641c641cb08480f8b5b80be795dc8206834
saizhang0218 <sai.zhang@mail.utoronto.ca>
2019-06-25 22:05:58 -0400

    Create fullAdder.v

---
+++ systolic_array/fullAdder.v
@@ -0,0 +1,6 @@
+module fullAdder(Sum,Cout,Cin,In1,In2);
+   input wire In1,In2,Cin;
+   output wire Sum,Cout;
+   assign Cout = (In1&In2)|(In1&Cin)|(In2&Cin);
+   assign Sum = In1^In2^Cin;
+endmodule
